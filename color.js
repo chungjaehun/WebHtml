8685e01a5ec94800ff23208cd3264305d960210c
재훈 정 <wognswjd70@naver.com>
2020-10-12 14:28:55 +0800

    객체 정의

---
+++ color.js
@@ -0,0 +1,37 @@
+
+var body = {
+      setColor:function(color){
+            document.querySelector('body').style.color = color;
+      },
+      setBackgroundColor:function(color){
+            document.querySelector('body').style.backgroundColor = color;
+      },
+}
+var Links = {
+      setColor: function(color){
+            var alist = document.querySelectorAll('a');
+            var i = 0;
+            while(i <alist.length){
+                  var element = alist[i];
+                  element.style.color=color;
+                  i=i+1;
+            }
+      }
+}
+
+function nightDayHandler(self){
+      var target = document.querySelector('body');
+      if (self.value == 'night'){
+            body.setBackgroundColor('black');
+            body.setColor('white');
+            self.value='day';
+
+            Links.setColor('powderblue');
+      } else{
+            body.setBackgroundColor('white');
+            body.setColor('black');
+            self.value='night';
+
+            Links.setColor('blue');
+      }
+}
